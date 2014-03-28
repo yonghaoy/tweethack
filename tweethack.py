439b895163a17105af0392ba1c46c01d6bf645d2
yonghaoy <yyhao1@gmail.com>
2014-03-28 15:50:42 -0400

    create my own version

--- tweethack.py
+++ tweethack.py
@@ -19,7 +19,17 @@ while quit != True:
       exit = raw_input("Type exit to go back or press enter to Tweet again.  ")
       if exit == "exit":
         exit = True
-  elif tos == "search":
+ elif tos == "tweet pictures":
+    print "Let's tweet a picture from the command line!"
+    
+    while exit != True:
+      tweet = raw_input("What would you like to tweet?  ")
+      photo = open('img/unc.jpg','rb')
+      twitter.update_status_with_media(status = tweet, media = photo)
+      exit = raw_input("Type exit to go back or press enter to Tweet again.  ")
+      if exit == "exit":
+        exit = True 
+ elif tos == "search":
     print "Let's search from the command line!"
     
     while exit != True:
